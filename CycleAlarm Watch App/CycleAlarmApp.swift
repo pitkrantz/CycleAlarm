66740454317460c453fba7598cbfc3337be3b3a7
pitkrantz <pitkrantz@gmail.com>
2023-01-29 16:11:40 +0100

    Initial Commit

---
+++ CycleAlarm Watch App/CycleAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleAlarmApp.swift
+//  CycleAlarm Watch App
+//
+//  Created by Pit Krantz on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CycleAlarm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
